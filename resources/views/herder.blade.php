eb26915389bf2786192fe98d1d2fa9ee6974e716
daxesh <daxbhandari20@gmail.com>
2022-10-20 16:10:11 +0530

    Add logout button and LogOut API Integrate

--- resources/views/herder.blade.php
+++ resources/views/herder.blade.php
@@ -8,15 +8,41 @@
 
 <body>
 
+    <button class="logout">Logout</button>
+
     <script>
         var token = localStorage.getItem('user_token');
         if (window.location.pathname == '/login' || window.location.pathname == '/register') {
             if (token != null) {
                 window.open('/profile', '_self');
             }
+            $('.logout').hide();
         } else {
             if (token == null) {
                 window.open('/login', '_self');
             }
         }
+
+        // Logout User
+
+        $(document).ready(function() {
+            $('.logout').click(function() {
+                $.ajax({
+                    url: "http://127.0.0.1:8000/api/logout",
+                    type: "GET",
+                    headers: {
+                        'Authorization': localStorage.getItem('user_token')
+                    },
+                    success: function(data) {
+                        if (data.success == true) {
+                            localStorage.removeItem('user_token');
+                            window.open("/login", '_self');
+                        } else {
+                            alert(data.msg);
+                        }
+                        console.log(data);
+                    }
+                })
+            });
+        });
     </script>
